42955fc96ffe5ad59f201dc8750842456b326bfe
arinmis <mustafa_arinmis@outlook.com>
2022-05-08 16:54:08 +0300

    Setup django rest framework

--- fast_rent/fast_rent/settings.py
+++ fast_rent/fast_rent/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'car_rent.apps.CarRentConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
