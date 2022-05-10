880bdf61467f811b1a2c8f65e0f14ec79c80aef5
Mustafa Arınmış <mustafa.arinmis@linarit.com>
2022-05-10 15:25:47 +0300

    Remove ignored files

--- fast_rent/car_rent/api/views.py
+++ fast_rent/car_rent/api/views.py
@@ -2,6 +2,7 @@ from django.http import  JsonResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view 
 
+
 @api_view(['GET'])
 def getRoutes(request):
     routes = [
