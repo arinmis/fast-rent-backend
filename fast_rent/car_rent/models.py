b9fe1c6105dd2f2178648e0ba1331d164c5d1b70
arinmis <mustafa_arinmis@outlook.com>
2022-05-10 15:25:47 +0300

    Remove ignored files

--- fast_rent/car_rent/models.py
+++ fast_rent/car_rent/models.py
@@ -1,16 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User as AuthUser, UserManager
 
 # Create your models here.
 
 
-class User(models.Model):
-    user_id = models.BigAutoField(primary_key=True)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    email = models.CharField(max_length=60)
-    password = models.CharField(max_length=40)
-    is_active = models.BooleanField(default=True)
-    personalID = models.CharField(max_length=40)
+class User(AuthUser):
+    class Meta:
+        db_table = 'User'
+    personalID = models.CharField(max_length=15)
 
 
 """
