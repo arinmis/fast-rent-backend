10dae7d006021007dbae0c63de3c75f9fd7accb2
Mustafa Arınmış <mustafa.arinmis@linarit.com>
2022-05-25 19:05:26 +0300

    Create new customer API created

--- backend/base/api/serializers.py
+++ backend/base/api/serializers.py
@@ -5,7 +5,25 @@ import base.models as models
 class UserSerializer(ModelSerializer):
     class Meta: 
         model = models.User
-        fields = ["first_name", "last_name", "email", "password"]
+        fields = ["username", "first_name", "last_name", "email", "password"]
+
+    # create new user
+    def create(self, validated_data):
+        user = models.User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
+        )
+
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
+
+    # update existing user
+    def update(self, validated_data):
+        user = models.User.objects.get(filter=validated_data["username"])
 
 class CustomerSerializer(ModelSerializer):
     user = UserSerializer() 
@@ -13,3 +31,32 @@ class CustomerSerializer(ModelSerializer):
     class Meta: 
         model = models.Customer
         fields = ["citizen_id", "user"] 
+
+    # create new 
+    def create(self, validated_data):
+        user_serializer = UserSerializer(data=validated_data.get("user")) 
+        # print(user_serializer.data)
+        if user_serializer.is_valid():
+            user = user_serializer.save()
+            customer = models.Customer.objects.create(
+                    user=user, 
+                    citizen_id = validated_data.get("citizen_id")
+                    )
+            print("here is user", user.username, user.first_name, user.last_name)
+            return customer 
+
+            
+
+
+"""
+{
+    "citizen_id": "101010101010",
+    "user": {
+        "username": "temp",
+        "first_name": "temp",
+        "last_name": "temp",
+        "email": "temp@example.com",
+        "password": "temp1234"
+    }
+}
+"""
