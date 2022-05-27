9146ae6285cc78a282806bd0b3a5170937c54d5f
arinmis <mustafa_arinmis@outlook.com>
2022-05-27 18:57:30 +0300

    Add optional id to reservation url

--- backend/base/api/views.py
+++ backend/base/api/views.py
@@ -93,10 +93,10 @@ example request
 """
 @api_view(['POST', 'GET'])
 # @permission_classes([IsAuthenticated])
-def reservation(request):
+def reservation(request, id = None):
     if request.method == "GET":
-        # reservations = models.Reservation.objects.filter(user_id=request.user.id)
-        reservations = models.Reservation.objects.filter(user_id=7)
+        reservations = models.Reservation.objects.filter(user_id=request.user.id)
+        reservations = models.Reservation.objects.filter(user_id=request.user.id)
         serializer = serializers.ReservationSerializer(reservations, many=True, context={"request": request})
         return Response(serializer.data)
     elif request.method == "POST":
@@ -112,4 +112,7 @@ def reservation(request):
         serializer.is_valid() # fix this: why data invalid 
         serializer.create(serializer.data)
         return Response("Reservation created")
+    elif request.method == "DELETE":
+        reservations = models.Reservation.objects.filter(user_id=request.user.id)
+        print("HERE", id)
 
