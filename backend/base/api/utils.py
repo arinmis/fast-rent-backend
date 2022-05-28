52ea01ae5a53dae35e46876c4af7b244e2e4621b
Mustafa Arınmış <mustafa.arinmis@linarit.com>
2022-05-28 17:33:40 +0300

    Filter already rented cars

--- backend/base/api/utils.py
+++ backend/base/api/utils.py
@@ -1,4 +1,4 @@
 import datetime
 
 def epochToDate(epoch):
-    return datetime.datetime.fromtimestamp(epoch).strftime('%d-%m-%Y')
+    return datetime.datetime.fromtimestamp(epoch).strftime('%Y-%m-%d')
--- backend/base/models.py
+++ backend/base/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import AbstractUser
 from django.conf import settings
 
 # Create your models here.
-
 class User(AbstractUser):
     pass
 
