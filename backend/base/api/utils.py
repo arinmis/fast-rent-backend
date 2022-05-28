0ede1a63cb8a0cf293ec798efe319b796aebbd3f
Mustafa Arınmış <mustafa.arinmis@linarit.com>
2022-05-28 19:42:07 +0300

    Deallocate car after 300 second

--- backend/base/api/utils.py
+++ backend/base/api/utils.py
@@ -4,5 +4,9 @@ import base.models as models
 def epochToDate(epoch):
     return datetime.datetime.fromtimestamp(epoch).strftime('%Y-%m-%d')
 
-def deallocate_car(car):
-    print("deallocate_car")
+# deacllocate car with given id 
+def deallocate_car(id):
+    car = models.Car.objects.get(pk=id)
+    car.is_allocated = False
+    car.save()
+    print("car.is_allocated", id, car.is_allocated)
